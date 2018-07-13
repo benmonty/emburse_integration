29cc1b1523f69a214f576e3604c3c379e49d38c1
Ben Montgomery <b3nm0nty@gmail.com>
2018-07-13 16:28:38 -0400

    use correct view for clear_cache

--- oauth/urls.py
+++ oauth/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^callback/', views.callback, name='callback'),
-    url(r'^clear_cache/', views.callback, name='clear_auth_cache'),
+    url(r'^clear_cache/', views.clear_auth_cache, name='clear_auth_cache'),
 ]
